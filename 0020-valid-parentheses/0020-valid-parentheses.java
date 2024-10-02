81db463b758261a49fa37219c4e59a568d57631c
rajavigneshwarank <rajavigneshwarank@gmail.com>
2024-10-03 00:43:47 +0530

    Time: 1 ms (98.54%), Space: 41.8 MB (16.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }
+            else if(c==')' || c=='}' || c==']'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if(!isMatchedPair(top,c)){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static boolean isMatchedPair(char o,char cl){
+       return (o=='(' && cl==')') || (o=='{' && cl=='}') || (o=='[' && cl==']');
+    }
+}
