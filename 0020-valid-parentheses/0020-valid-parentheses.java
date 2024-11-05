3ea5ebcbac44eea992fe46d98290d62e7ed2adeb
rajavigneshwarank <rajavigneshwarank@gmail.com>
2024-11-05 10:56:55 +0530

    Time: 2 ms (97.06%), Space: 41.5 MB (63.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,23 @@
 class Solution {
-    public boolean isValid(String s) {
+    public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(c=='(' || c=='{' || c=='['){
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            }
-            else if(c==')' || c=='}' || c==']'){
-                if(stack.isEmpty()){
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
                     return false;
                 }
                 char top = stack.pop();
-                if(!isMatchedPair(top,c)){
+                if (!isMatchedPair(top, c)) {
                     return false;
                 }
             }
         }
         return stack.isEmpty();
     }
-    public static boolean isMatchedPair(char o,char cl){
-       return (o=='(' && cl==')') || (o=='{' && cl=='}') || (o=='[' && cl==']');
+
+    public static boolean isMatchedPair(char o, char cl) {
+        return (o == '(' && cl == ')') || (o == '{' && cl == '}') || (o == '[' && cl == ']');
     }
 }
