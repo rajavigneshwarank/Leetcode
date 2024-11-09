df62a6d3e13dc18345ec71b31231eb441ca5b48f
rajavigneshwarank <rajavigneshwarank@gmail.com>
2024-11-09 15:09:48 +0530

    Time: 170 ms (5.03%), Space: 45.6 MB (96.68%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+
+            ans[i]=-1;
+            int j = (i+1)%n;
+
+            while(i!=j){
+                if(nums[j]>nums[i]){
+                    ans[i]=nums[j];
+                    break;
+                }
+                else{
+                    j = (j+1)%nums.length;
+                }
+            }
+        }
+        return ans;
+    }
+}
